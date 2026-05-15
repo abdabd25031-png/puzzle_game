c8e2e90cde597bcb9def9fd65109b94bf024195e
abdulrahman <abd.abd25031@coai.uobaghdad.edu.iq>
2026-05-15 20:27:57 +0300

    pictures

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,36 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
 
-void main() {
-  runApp(const PuzzleGame());
-}
+void main() => runApp(const PuzzleGame());
 
 class GameController {
   List<int> board = [];
 
   void shuffleBoard() {
-    board = List.generate(16, (index) => index ~/ 2);
-    board.shuffle(Random());
+    board = List.generate(16, (index) => index);
+    board.shuffle();
+  }
+
+  bool movePiece(int index) {
+    int emptyIndex = board.indexOf(15);
+
+    if (_isAdjacent(index, emptyIndex)) {
+      int temp = board[index];
+      board[index] = board[emptyIndex];
+      board[emptyIndex] = temp;
+      return true;
+    }
+
+    return false;
+  }
+
+  bool _isAdjacent(int idx1, int idx2) {
+    int row1 = idx1 ~/ 4;
+    int col1 = idx1 % 4;
+
+    int row2 = idx2 ~/ 4;
+    int col2 = idx2 % 4;
+
+    return (row1 - row2).abs() + (col1 - col2).abs() == 1;
   }
 }
 
@@ -21,26 +41,22 @@ class PuzzleGame extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+      ),
       home: HomePage(),
     );
   }
 }
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Puzzle Game"),
-        centerTitle: true,
-        backgroundColor: const Color(0xFF6DD5ED),
-      ),
-
       body: Container(
         width: double.infinity,
-
         decoration: const BoxDecoration(
           gradient: LinearGradient(
             colors: [
@@ -51,12 +67,9 @@ class HomePage extends StatelessWidget {
             end: Alignment.bottomCenter,
           ),
         ),
-
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-
           children: [
-
             const Icon(
               Icons.extension,
               size: 100,
@@ -66,7 +79,7 @@ class HomePage extends StatelessWidget {
             const SizedBox(height: 20),
 
             const Text(
-              "Welcome to Puzzle Game",
+              "Puzzle Game",
               style: TextStyle(
                 fontSize: 32,
                 fontWeight: FontWeight.bold,
@@ -77,29 +90,20 @@ class HomePage extends StatelessWidget {
 
             ElevatedButton(
               onPressed: () {
-
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => CategoriesPage(),
+                    builder: (context) => const CategoriesPage(),
                   ),
                 );
-
               },
-
               style: ElevatedButton.styleFrom(
                 backgroundColor: const Color(0xFF4DD0E1),
-
                 padding: const EdgeInsets.symmetric(
                   horizontal: 40,
                   vertical: 15,
                 ),
-
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
-                ),
               ),
-
               child: const Text(
                 "Start Game",
                 style: TextStyle(
@@ -116,193 +120,183 @@ class HomePage extends StatelessWidget {
 }
 
 class CategoriesPage extends StatelessWidget {
+  const CategoriesPage({super.key});
 
-final List<Map<String, String>> categories = [
-
-{
-"title": "Animals",
-"emoji": "🐶",
-},
-
-{
-"title": "Anime",
-"emoji": "🎌",
-},
-
-{
-"title": "Cars",
-"emoji": "🚗",
-},
-
-{
-"title": "Football",
-"emoji": "⚽",
-},
-
-];
-
-@override
-Widget build(BuildContext context) {
-
-return Scaffold(
-appBar: AppBar(
-title: const Text("Categories"),
-centerTitle: true,
-backgroundColor: const Color(0xFF6DD5ED),
-),
-
-body: Padding(
-padding: const EdgeInsets.all(20),
-
-child: GridView.builder(
-
-itemCount: categories.length,
-
-gridDelegate:
-const SliverGridDelegateWithFixedCrossAxisCount(
-
-crossAxisCount: 2,
-crossAxisSpacing: 20,
-mainAxisSpacing: 20,
-
-),
-
-itemBuilder: (context, index) {
-
-return GestureDetector(
-
-onTap: () {
-
-Navigator.push(
-context,
-MaterialPageRoute(
-builder: (context) => GamePage(
-categoryName:
-categories[index]["title"]!,
-),
-),
-);
-
-},
-
-child: Container(
-
-decoration: BoxDecoration(
-color: const Color(0xFFB2EBF2),
-borderRadius: BorderRadius.circular(25),
-),
-
-  child: Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-
-    children: [
-
-      Text(
-        categories[index]["emoji"]!,
-        style: const TextStyle(
-          fontSize: 50,
-        ),
-      ),
-
-      const SizedBox(height: 10),
-
-      Text(
-        categories[index]["title"]!,
-        style: const TextStyle(
-          fontSize: 24,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-    ],
-  ),
-),
-);
-},
-),
-),
-);
-}
-}
-
-class GamePage extends StatelessWidget {
-
-  final String categoryName;
-
-  GamePage({
-    required this.categoryName,
-  });
-
-  final GameController controller = GameController();
+  final List<Map<String, String>> categories = const [
+    {"title": "Animals", "emoji": "🐶"},
+    {"title": "Anime", "emoji": "🎌"},
+    {"title": "Cars", "emoji": "🚗"},
+    {"title": "Football", "emoji": "⚽"},
+  ];
 
   @override
   Widget build(BuildContext context) {
-
-    controller.shuffleBoard();
-
     return Scaffold(
       appBar: AppBar(
-        title: Text(categoryName),
-        centerTitle: true,
+        title: const Text("Select Category"),
         backgroundColor: const Color(0xFF6DD5ED),
       ),
-
-      body: Padding(
+      body: GridView.builder(
         padding: const EdgeInsets.all(20),
-
-        child: GridView.builder(
-
-          itemCount: controller.board.length,
-
-          gridDelegate:
-          const SliverGridDelegateWithFixedCrossAxisCount(
-
-            crossAxisCount: 4,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-
-          ),
-
-          itemBuilder: (context, index) {
-
-            return Container(
-
-              decoration: BoxDecoration(
-                color: const Color(0xFF80DEEA),
-                borderRadius: BorderRadius.circular(15),
-              ),
-
-              child: Center(
-
-
-                  child: Text(
-                    getEmoji(controller.board[index]),
-                    style: const TextStyle(
-                      fontSize: 40,
-                    ),
+        itemCount: categories.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => GamePage(
+                    categoryName: categories[index]["title"]!,
                   ),
                 ),
+              );
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                color: const Color(0xFFB2EBF2),
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    categories[index]["emoji"]!,
+                    style: const TextStyle(fontSize: 50),
+                  ),
+
+                  const SizedBox(height: 10),
+
+                  Text(
+                    categories[index]["title"]!,
+                    style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class GamePage extends StatefulWidget {
+  final String categoryName;
+
+  const GamePage({
+    super.key,
+    required this.categoryName,
+  });
+
+  @override
+  State<GamePage> createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+  final GameController controller = GameController();
+
+  @override
+  void initState() {
+    super.initState();
+    controller.shuffleBoard();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.categoryName),
+        backgroundColor: const Color(0xFF6DD5ED),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: GridView.builder(
+          itemCount: controller.board.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 4,
+            crossAxisSpacing: 5,
+            mainAxisSpacing: 5,
+          ),
+          itemBuilder: (context, index) {
+
+            int val = controller.board[index];
+
+            if (val == 15) {
+              return Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              );
+            }
+
+            String imagePath = "";
+
+            if (widget.categoryName == "Animals") {
+              imagePath =
+              "assets/animals/PineTools.com_files/$val.jpg";
+            }
+            else if (widget.categoryName == "Anime") {
+              imagePath =
+              "assets/Anime/PineTools.com_files/$val.jpg";
+            }
+            else if (widget.categoryName == "Cars") {
+              imagePath =
+              "assets/Cars/PineTools.com_files/$val.jpg";
+            }
+            else if (widget.categoryName == "Football") {
+              imagePath =
+              "assets/Football/PineTools.com_files/$val.jpg";
+            }
+
+            return GestureDetector(
+              onTap: () {
+                if (controller.movePiece(index)) {
+                  setState(() {});
+                }
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  border: Border.all(
+                    color: Colors.white,
+                    width: 1,
+                  ),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Image.asset(
+                    imagePath,
+                    fit: BoxFit.cover,
+                    errorBuilder: (
+                        context,
+                        error,
+                        stackTrace,
+                        ) {
+                      return Center(
+                        child: Text(
+                          "$val",
+                          style: const TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
             );
           },
         ),
       ),
     );
-  }
-  String getEmoji(int number) {
-
-    List<String> emojis = [
-
-      "🐶",
-      "🐱",
-      "🦁",
-      "🐸",
-      "🐼",
-      "🐵",
-      "🐰",
-      "🦊",
-
-    ];
-
-    return emojis[number];
-
   }
 }
